1f4558610c6d5bfa1de33be869bf161fe492b704
sidd-hiver <siddharth@grexit.com>
2018-12-20 17:55:20 +0530

    remove log

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ module.exports = app => {
   app.log('Yay, the app was loaded!')
 
   app.on('pull_request.opened', async context => {
-    console.log('inside pull');
     const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
     return context.github.issues.createComment(issueComment)
   })
